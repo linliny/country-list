11e273a187f30ec22364e061d71a8e1e98eecbd5
levin.lin <levin.lin@atck.io>
2018-06-22 17:51:03 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-COUNTRY-LIST
+# country-list（中文简体，中文繁体）
+
+## Example
+
+``` js
+var countries = require('country-list-l');
+
+console.log(countries.getCountryList().length); // Iceland
+console.log(countries.getCountryByName('China')); // IS
+```
+
+lang: 默认英文，简体-'cn'，繁体-'tc'
+
+## Methods
+
+### getCountryList(lang)
+
+返回所有国家和地区列表
+
+### getUNCountryList(lang)
+
+返回所有国家列表（联合国会员国）
+
+### getCountryByName(name,lang)
+
+通过国家名称查询国家信息
+
+## Install
+
+``` cli
+npm install country-list-l
+```
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -1,3 +1,75 @@
 const country = require('./country.json')
 
-console.log(country.UNSC.length)
+class Country {
+    constructor() {
+        this.area2lang = {
+            cn: 'chineseName',
+            tc: 'traditionalName',
+            en: 'name'
+        }
+    }
+
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new Country()
+        }
+        return this.instance
+    }
+
+    getQuickSearch() {
+        if (!this.quickSearch) {
+            const findByEn = {}
+            const findByCn = {}
+            const findByTc = {}
+            for (let i = 0, n = country.length; i < n; i++) {
+                findByEn[country[i].name] = i
+                findByCn[country[i].chineseName] = i
+                findByTc[country[i].traditionalName] = i
+            }
+            this.quickSearch = {
+                chineseName: findByCn,
+                name: findByEn,
+                traditionalName: findByTc
+            }
+        }
+        return this.quickSearch
+    }
+
+    getCountryList(language) {
+        if (!this.countryList) this.countryList = {}
+
+        const lang = this.area2lang[language] || 'name'
+        if (!this.countryList[lang]) {
+            const list = []
+            for (let val of country) {
+                list.push(val[lang])
+            }
+            this.countryList[lang] = list
+        }
+
+        return this.countryList[lang]
+    }
+
+    getUNCountryList(language) {
+        if (!this.UNCL) this.UNCL = {}
+
+        const lang = this.area2lang[language] || 'name'
+        if (!this.UNCL[lang]) {
+            const list = []
+            for (let val of country) {
+                if (val.type === 'UNSC') list.push(val[lang])
+            }
+            this.UNCL[lang] = list
+        }
+
+        return this.UNCL[lang]
+    }
+
+    getCountryByName(name, language) {
+        const lang = this.area2lang[language] || 'name'
+        const result = country[this.getQuickSearch()[lang][name]]
+        return result || 'not exist'
+    }
+}
+
+module.exports = Country.getInstance()
